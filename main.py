c5c8c2a2ec62eb1687ae61dd5516f8345d2f5713
Joshua Jakowlew <jakowlew@pm.me>
2019-07-07 15:20:37 +0300

    added timing listing

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def main():
         perform(config)
 def perform(config):
     print("Config loaded")
+    for key in config["keys"]:
+        print("key {}, timings from {}s to {}s".format(
+            key["key"],
+            key["delay"]["min"],
+            key["delay"]["max"]
+        ))
     print("Trigger key is \"{}\"".format(config["trigger"]))
     while True:
         autocast.perform(config)
